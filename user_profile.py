c0b940991acbfbe887ea17944df009af4e1bcb89
Julia Furuguem <133160004+juliafuruguem@users.noreply.github.com>
2025-05-14 17:26:39 -0300

    Delete user_profile.py

--- user_profile.py
+++
@@ -1,9 +0,0 @@
-def calculate_average_glucose(glucose_readings):
-
-    if not glucose_readings:
-        raise ValueError("A lista de leituras não pode estar vazia.")
-    
-    if not all(isinstance(g, (int, float)) for g in glucose_readings):
-        raise ValueError("Todos os valores devem ser números.")
-    
-    return sum(glucose_readings) / len(glucose_readings)
